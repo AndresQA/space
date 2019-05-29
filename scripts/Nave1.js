58096d6cf5907e0e7c2adaf81803086ee6044791
AndresQA <34174822+AndresQA@users.noreply.github.com>
2019-05-29 00:40:51 -0500

    Update Nave1.js

--- scripts/Nave1.js
+++ scripts/Nave1.js
@@ -3,7 +3,7 @@ class Nave1 extends Enemigo{
     constructor(app){
        super(app);
         for (let i = 0; i < 4; i++) {
-           this.imagen.push(this.app.loadImage('/imgs/Enemigo 1.png'));
+           this.imagen.push(this.app.loadImage('./imgs/Enemigo 1.png'));
             
         }
     }
